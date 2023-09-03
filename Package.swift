37782793119f413eea05939e0b081ed488e55f53
Jin Mok <102392728+mokkang@users.noreply.github.com>
2023-09-03 00:07:35 -0700

    Create Package.swift

---
+++ Package.swift
@@ -0,0 +1,32 @@
+// swift-tools-version: 5.7
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+	name: "gen-ir",
+	platforms: [
+		.macOS(.v12)
+	],
+	dependencies: [
+		// Dependencies declare other packages that this package depends on.
+		// .package(url: /* package url */, from: "1.0.0"),
+		.package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.0.0"),
+		.package(url: "https://github.com/apple/swift-log.git", from: "1.0.0")
+	],
+	targets: [
+		// Targets are the basic building blocks of a package. A target can define a module or a test suite.
+		// Targets can depend on other targets in this package, and on products in packages this package depends on.
+		.executableTarget(
+			name: "gen-ir",
+			dependencies: [
+				.product(name: "ArgumentParser", package: "swift-argument-parser"),
+				.product(name: "Logging", package: "swift-log")
+			]
+		),
+		.testTarget(
+			name: "gen-irTests",
+			dependencies: ["gen-ir"]
+		)
+	]
+)
